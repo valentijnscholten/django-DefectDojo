59aaffe2ecd654deae135376b7311376264041b7
Valentijn Scholten <valentijnscholten@gmail.com>
2020-01-19 17:10:28 +0100

    debug: add debug toolbar urls

--- dojo/urls.py
+++ dojo/urls.py
@@ -198,7 +198,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
     urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
 
+import debug_toolbar
 urlpatterns += [url(r"^__debug__/", include(debug_toolbar.urls))]
