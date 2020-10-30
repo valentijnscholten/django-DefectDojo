167735bc906b851dc988eb7cf913c594ec61cc59
Valentijn Scholten <valentijnscholten@gmail.com>
2020-10-30 15:12:12 +0100

    settings: normalize

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -4,13 +4,13 @@ from datetime import timedelta
 from celery.schedules import crontab
 
 import environ
-root = environ.Path(__file__) - 3  # Three folders back!
+root = environ.Path(__file__) - 3  # Three folders back
 
 # reference: https://pypi.org/project/django-environ/
 env = environ.Env(
     # Set casting and default values
     DD_SITE_URL=(str, 'http://localhost:8080'),
-    DD_DEBUG=(bool, True),
+    DD_DEBUG=(bool, False),
     DD_DJANGO_METRICS_ENABLED=(bool, False),
     DD_LOGIN_REDIRECT_URL=(str, '/'),
     DD_DJANGO_ADMIN_ENABLED=(bool, False),
@@ -124,9 +124,9 @@ env = environ.Env(
     }),
     # merging findings doesn't always work well with dedupe and reimport etc.
     # disable it if you see any issues (and report them on github)
-    DD_DISABLE_FINDING_MERGE=(bool, True),
-    # Set to True if you want to allow authorized users to make changes to findings or delete them    
-    DD_AUTHORIZED_USERS_ALLOW_CHANGE=(bool, True),
+    DD_DISABLE_FINDING_MERGE=(bool, False),
+    # Set to True if you want to allow authorized users to make changes to findings or delete them
+    DD_AUTHORIZED_USERS_ALLOW_CHANGE=(bool, False),
     DD_AUTHORIZED_USERS_ALLOW_DELETE=(bool, False),
     # Set to True if you want to allow authorized users staff access only on specific products
     # This will only apply to users with 'active' status
@@ -384,9 +384,6 @@ SOCIAL_AUTH_TRAILING_SLASH = env('DD_SOCIAL_AUTH_TRAILING_SLASH')
 SAML2_ENABLED = env('DD_SAML2_ENABLED')
 SAML2_LOGOUT_URL = env('DD_SAML2_LOGOUT_URL')
 SAML2_AUTH = {
-    # Metadata is required, choose either remote url or local file path
-    'METADATA_AUTO_CONF_URL': env('DD_SAML2_METADATA_AUTO_CONF_URL'),
-    'METADATA_LOCAL_FILE_PATH': env('DD_SAML2_METADATA_LOCAL_FILE_PATH'),
     'ASSERTION_URL': env('DD_SAML2_ASSERTION_URL'),
     'ENTITY_ID': env('DD_SAML2_ENTITY_ID'),
     # Optional settings below
@@ -603,15 +600,12 @@ INSTALLED_APPS = (
     'django_celery_results',
     'social_django',
     'drf_yasg2',
-    'debug_toolbar',
-    # 'cachalot'
 )
 
 # ------------------------------------------------------------------------------
 # MIDDLEWARE
 # ------------------------------------------------------------------------------
 DJANGO_MIDDLEWARE_CLASSES = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'dojo.middleware.DojoSytemSettingsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -624,7 +618,7 @@ DJANGO_MIDDLEWARE_CLASSES = [
     'social_django.middleware.SocialAuthExceptionMiddleware',
     'watson.middleware.SearchContextMiddleware',
     'auditlog.middleware.AuditlogMiddleware',
-    'crum.CurrentRequestUserMiddleware',    
+    'crum.CurrentRequestUserMiddleware',
 ]
 
 MIDDLEWARE = DJANGO_MIDDLEWARE_CLASSES
@@ -683,10 +677,10 @@ CELERY_BEAT_SCHEDULE = {
         'task': 'dojo.tasks.async_update_findings_from_source_issues',
         'schedule': timedelta(hours=3),
     },
-#    'compute-sla-age-and-notify': {
-#        'task': 'dojo.tasks.async_sla_compute_and_notify',
-#        'schedule': crontab(hour=7, minute=30),
-#    }
+    'compute-sla-age-and-notify': {
+        'task': 'dojo.tasks.async_sla_compute_and_notify',
+        'schedule': crontab(hour=7, minute=30),
+    }
 }
 
 
@@ -857,7 +851,6 @@ LOGGING = {
         },
         'console': {
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
         },
         'json_console': {
             'class': 'logging.StreamHandler',
@@ -904,11 +897,6 @@ LOGGING = {
             'level': 'WARNING',
             'propagate': False,
         },
-        'django.db.backends': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        }
-
     }
 }
 
@@ -921,35 +909,3 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 
 # Maximum size of a scan file in MB
 SCAN_FILE_MAX_SIZE = 100
-
-# django-tagging uses raw queries underneath breaking ORM query caching
-CACHALOT_UNCACHABLE_TABLES = frozenset(('django_migrations'))
-
-
-def show_toolbar(request):
-    return True
-
-
-DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
-    "INTERCEPT_REDIRECTS": False,
-    "SHOW_COLLAPSED": True,
-}
-
-DEBUG_TOOLBAR_PANELS = [
-    'ddt_request_history.panels.request_history.RequestHistoryPanel',  # Here it is
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    # 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-    # 'cachalot.panels.CachalotPanel',
-]
